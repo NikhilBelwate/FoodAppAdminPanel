20058e1ba6009e12676166677183e96b4d6850d1
ronakparasrampuria <52551554+ronakparasrampuria@users.noreply.github.com>
2020-06-30 23:15:08 -0700

    Dairy Sub Category Committed

--- food-app-admin/src/app/Dairy/edit-dairy-subcategory/edit-dairy-subcategory.component.ts
+++ food-app-admin/src/app/Dairy/edit-dairy-subcategory/edit-dairy-subcategory.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { SubCategory } from 'src/model/SubCategory';
+import { Grocerycategory, Dairycategory } from 'src/app/data-interfaces';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DataApiService } from 'src/app/data-api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-dairy-subcategory',
@@ -7,9 +12,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditDairySubcategoryComponent implements OnInit {
 
-  constructor() { }
+  @Input() public subCategory:SubCategory;
+  public categoryService:Dairycategory;
+  public editFlag:boolean=false;
+  constructor(private fb:FormBuilder,private dataServiceApi:DataApiService,private router : Router) { }
+
+
+  editSubcategoryProfileForm = this.fb.group({
+    subCategoryId:[''],
+    SubCategoryName:['',[Validators.required]],
+    categoryId:[''],
+    SubCategoryPrice:['',[Validators.required,Validators.pattern(/[0-9]/g)]],
+    SubCategoryUrl:['',[Validators.required]],
+    SubCategoryDesc:['',[Validators.required]],
+    LocationId:[''],
+    subCategoryTax:['',[Validators.required,Validators.pattern(/[0-9]/g)]],
+    subCategoryUnit:['',[Validators.required]],
+    CategoryName:['',[Validators.required]]
+  })
 
   ngOnInit(): void {
+    console.log(this.subCategory);
+    this.getCategoryList();
+    // set the value of dropdown
+    this.editSubcategoryProfileForm.get('CategoryName').setValue(this.subCategory.CategoryName);
   }
+
+  getCategoryList() {
+    this.dataServiceApi.getDairyCategoryDetailsApi().subscribe(
+      data=>{
+        this.categoryService = data;
+        console.log(this.categoryService);
+      }
+    )
+  }
+  // Message Event is used to change flag status from child component to parent component
+  @Output() messageEvent = new EventEmitter<Boolean>();
+  onSubmit(){
+    // console.warn(this.userProfileForm.value);
+    const categoryName = this.editSubcategoryProfileForm.get('CategoryName').value;
+    const categoryId = this.categoryService.categoryList.find(x=>x.CategoryName===categoryName).DairyCategoryId
+    this.subCategory.CategoryId = categoryId;
+      this.dataServiceApi.updateDairySubCategoryDetailsApi(this.subCategory).subscribe(
+       data =>{
+         console.log(data);
+         alert("Sub Category Records Updated Succesfully");
+         this.editFlag = false;
+         this.messageEvent.emit(this.editFlag);
+         this.router.navigate(["/dairysubCategory"]);
+       },
+       error => {
+       }
+       
+     )
+   }
 
 }
